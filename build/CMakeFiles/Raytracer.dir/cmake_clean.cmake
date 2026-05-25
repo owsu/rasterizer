c66114812b9db30a6dea2a302c531c231583f393
Owusu <fullstackowusu@gmail.com>
2026-05-25 18:04:45 -0500

    Rudimentary raytracer with red sphere

---
+++ build/CMakeFiles/Raytracer.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Raytracer.dir/main.cpp.obj"
+  "CMakeFiles/Raytracer.dir/main.cpp.obj.d"
+  "Raytracer.exe"
+  "Raytracer.exe.manifest"
+  "Raytracer.pdb"
+  "libRaytracer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Raytracer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
